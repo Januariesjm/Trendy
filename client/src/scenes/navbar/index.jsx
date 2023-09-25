886fb65d42f4adba4ea5ab1e9879d5518cabb426
Januaries mwaka Mawioo <januaries.jm98@gmail.com>
2023-09-25 22:59:15 +0300

    trendy logo

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Trendy
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
